5f3f76490f2a677f5370b067b92e4525c2e8ceba
Matti Jokitulppo <melonmanchan@gmail.com>
2017-03-07 16:23:48 +0200

    build authenticated client

--- app/src/main/java/fi/tutee/tutee/data/source/TuteeRepository.java
+++ app/src/main/java/fi/tutee/tutee/data/source/TuteeRepository.java
@@ -1,38 +1,83 @@
 package fi.tutee.tutee.data.source;
 
-/**
- * Created by mat on 06/03/2017.
- */
+import fi.tutee.tutee.data.LoginRequest;
+import fi.tutee.tutee.data.RegisterRequest;
+import fi.tutee.tutee.data.User;
+import fi.tutee.tutee.data.source.local.TuteeLocalDataSource;
+import fi.tutee.tutee.data.source.remote.TuteeRemoteDataSource;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class TuteeRepository  implements TuteeDataSource {
+public class TuteeRepository implements TuteeDataSource {
 
     private static TuteeRepository instance = null;
 
-    private  final TuteeDataSource remote;
+    private final TuteeRemoteDataSource remote;
 
-    private  final TuteeDataSource local;
+    private final TuteeLocalDataSource local;
 
-    public TuteeRepository(TuteeDataSource remote, TuteeDataSource local) {
+    private User loggedInUser;
+
+    public User getLoggedInUser() {
+        return loggedInUser;
+    }
+
+    public TuteeRepository(TuteeRemoteDataSource remote, TuteeLocalDataSource local) {
         this.remote = remote;
         this.local = local;
     }
 
-    public static TuteeRepository getInstance(TuteeDataSource remote,
-                                              TuteeDataSource local) {
+    public static TuteeRepository getInstance(TuteeRemoteDataSource remote,
+                                              TuteeLocalDataSource local) {
         if (instance == null) {
             instance = new TuteeRepository(remote, local);
         }
         return instance;
     }
 
-
     @Override
-    public void basicLogin(String email, String password) {
+    public void basicLogin(LoginRequest req, final Callback<User> cb) {
 
+        remote.basicLogin(req, new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response.isSuccessful()) {
+                    loggedInUser = response.body();
+                }
+
+                cb.onResponse(call, response);
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                cb.onFailure(call, t);
+            }
+        });
     }
 
     @Override
     public void googleLogin(String token) {
 
+    }
+
+    @Override
+    public void register(RegisterRequest req, final Callback<User> cb) {
+
+        remote.register(req, new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response.isSuccessful()) {
+                    loggedInUser = response.body();
+                }
+
+                cb.onResponse(call, response);
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                cb.onFailure(call, t);
+            }
+        });
     }
 }
--- app/src/main/java/fi/tutee/tutee/data/source/remote/TuteeRemoteDataSource.java
+++ app/src/main/java/fi/tutee/tutee/data/source/remote/TuteeRemoteDataSource.java
@@ -1,9 +1,14 @@
 package fi.tutee.tutee.data.source.remote;
 
+import java.io.IOException;
+
 import fi.tutee.tutee.data.LoginRequest;
 import fi.tutee.tutee.data.RegisterRequest;
 import fi.tutee.tutee.data.User;
 import fi.tutee.tutee.data.source.TuteeDataSource;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -17,11 +22,7 @@ public class TuteeRemoteDataSource implements TuteeDataSource {
     private TuteeService service;
 
     public TuteeRemoteDataSource() {
-        retrofit = new Retrofit.Builder()
-                .baseUrl("https://apartheidfun.club")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
+        retrofit = buildUnauthenticatedRetrofit();
         service = retrofit.create(TuteeService.class);
 
     }
@@ -32,6 +33,49 @@ public class TuteeRemoteDataSource implements TuteeDataSource {
         }
 
         return instance;
+    }
+
+    public void setToken(String authToken) {
+        retrofit = buildAuthenticatedRetrofit(authToken);
+        this.service = retrofit.create(TuteeService.class);
+    }
+
+    private Retrofit buildAuthenticatedRetrofit(final String authToken) {
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request original = chain.request();
+
+                Request request = original.newBuilder()
+                        .header("Authorization", "Bearer: " + authToken)
+                        .method(original.method(), original.body())
+                        .build();
+
+                return chain.proceed(request);
+            }
+        });
+
+        OkHttpClient client = httpClient.build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://apartheidfun.club")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
+
+    private Retrofit buildUnauthenticatedRetrofit () {
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://apartheidfun.club")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return retrofit;
     }
 
     @Override
