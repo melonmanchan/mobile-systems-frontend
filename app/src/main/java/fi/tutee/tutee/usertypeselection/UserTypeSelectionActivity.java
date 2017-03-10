bb0c9272113f45bfdf58a97926fc911a76fb88b4
Emmi Linkola <emmi.linkola@aalto.fi>
2017-03-10 14:24:57 +0200

    Added icons and top menu bar

--- app/src/main/java/fi/tutee/tutee/usertypeselection/UserTypeSelectionActivity.java
+++ app/src/main/java/fi/tutee/tutee/usertypeselection/UserTypeSelectionActivity.java
@@ -1,7 +1,12 @@
 package fi.tutee.tutee.usertypeselection;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.Window;
 
 import fi.tutee.tutee.R;
 import fi.tutee.tutee.TuteeApplication;
@@ -9,10 +14,20 @@ import fi.tutee.tutee.utils.ActivityUtils;
 
 public class UserTypeSelectionActivity extends AppCompatActivity {
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_type_selection);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.show();
 
         TuteeApplication app = (TuteeApplication)  getApplication();
 
