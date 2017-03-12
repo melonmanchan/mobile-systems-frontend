224dec35c411cd690adc9b75bca3a668c1bd3c26
Matti Jokitulppo <melonmanchan@gmail.com>
2017-03-12 23:15:21 +0200

    Initial work on tabbed home view

--- app/src/main/java/fi/tutee/tutee/home/HomeFragment.java
+++ app/src/main/java/fi/tutee/tutee/home/HomeFragment.java
@@ -1,13 +1,19 @@
 package fi.tutee.tutee.home;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import fi.tutee.tutee.R;
 
@@ -18,6 +24,8 @@ import fi.tutee.tutee.R;
 public class HomeFragment extends Fragment implements HomeContract.View {
     private HomeContract.Presenter presenter;
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     public static HomeFragment newInstance() {
         Bundle arguments = new Bundle();
@@ -38,6 +46,13 @@ public class HomeFragment extends Fragment implements HomeContract.View {
 
         View root = inflater.inflate(R.layout.content_home, container, false);
 
+        viewPager = (ViewPager) root.findViewById(R.id.homeViewPager);
+        tabLayout = (TabLayout) root.findViewById(R.id.homeTabs);
+
+        setupViewPager(viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
         setHasOptionsMenu(true);
         return root;
     }
@@ -50,5 +65,41 @@ public class HomeFragment extends Fragment implements HomeContract.View {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getFragmentManager());
+        adapter.addFragment(new HomeScheduleFragment(), "Schedule");
+        adapter.addFragment(new HomeMessagesFragment(), "Messages");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
---
+++ app/src/main/java/fi/tutee/tutee/home/HomeScheduleFragment.java
@@ -0,0 +1,27 @@
+package fi.tutee.tutee.home;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fi.tutee.tutee.R;
+
+public class HomeScheduleFragment extends Fragment{
+    public HomeScheduleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.content_home_schedule, container, false);
+    }
+}
