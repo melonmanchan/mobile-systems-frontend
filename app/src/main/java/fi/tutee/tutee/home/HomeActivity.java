4d81c919cc25d502c3370c1f9a1dd6a5c0f9fe06
Matti Jokitulppo <melonmanchan@gmail.com>
2017-03-12 22:32:01 +0200

    Show action bar in home

--- app/src/main/java/fi/tutee/tutee/home/HomeActivity.java
+++ app/src/main/java/fi/tutee/tutee/home/HomeActivity.java
@@ -2,6 +2,7 @@ package fi.tutee.tutee.home;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 import fi.tutee.tutee.R;
 import fi.tutee.tutee.TuteeApplication;
@@ -12,6 +13,14 @@ import fi.tutee.tutee.utils.ActivityUtils;
  */
 
 public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
