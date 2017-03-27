9929d541e0de798baee4a9fdbef101aea5b0ee1d
lehtone1 <eero.lehtonen@aalto.fi>
2017-03-27 21:09:53 +0300

    Calendar

--- app/src/main/java/fi/tutee/tutee/home/HomeScheduleFragment.java
+++ app/src/main/java/fi/tutee/tutee/home/HomeScheduleFragment.java
@@ -1,14 +1,25 @@
 package fi.tutee.tutee.home;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 import fi.tutee.tutee.R;
 
 public class HomeScheduleFragment extends Fragment{
+    public WeekView mWeekView;
+    public TabLayout days;
     public HomeScheduleFragment() {
         // Required empty public constructor
     }
@@ -21,7 +32,52 @@ public class HomeScheduleFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.content_home_schedule, container, false);
+        // Get a reference for the week view in the layout.
+        mWeekView = (WeekView) root.findViewById(R.id.weekView);
+        days = (TabLayout) root.findViewById(R.id.days);
+
+        days.addTab(days.newTab().setText("Ding"));
+
+
+        // Set an action when any event is clicked.
+        //mWeekView.setOnEventClickListener(mEventClickListener);
+
+        // The week view has infinite scrolling horizontally. We have to provide the events of a
+        // month every time the month changes on the week view.
+        mWeekView.setMonthChangeListener(mMonthChangeListener);
+
+        // Set long press listener for events.
+        //mWeekView.setEventLongPressListener(mEventLongPressListener)
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.content_home_schedule, container, false);
+
+        Calendar date = Calendar.getInstance();
+        date.add(Calendar.DAY_OF_WEEK, 100);
+        mWeekView.goToDate(date);
+        return root;
+
+
+
+
     }
+
+    MonthLoader.MonthChangeListener mMonthChangeListener = new MonthLoader.MonthChangeListener() {
+        @Override
+        public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+            // Populate the week view with some events.
+            List<WeekViewEvent> events = getEvents(newYear, newMonth);
+            return events;
+        }
+    };
+
+    private List<WeekViewEvent> getEvents(int newYear, int newMonth) {
+
+        List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+        return events;
+    }
+
+
 }
